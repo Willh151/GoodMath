707eb87aca26c91e0b7418f05f5d1f09375bce4f
Will Hancock <will.hancock@sinclair.edu>
2024-11-14 09:54:01 -0500

    average function fixed main fixed added input validation

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,19 +1,43 @@
+// Will Hancock
+// November 14, 2024
+// CIS 1202 501
+
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 float average(int i1, int i2, int i3)
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;
+}
+
+int getValidatedInput(const string& prompt)
+{
+	int num;
+	while (true)
+	{
+		cout << prompt;
+		cin >> num;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input. Please enter an integer." << endl;
+		}
+		else {
+			break;
+		}
+	}
+	return num;
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+
+	int n1 = getValidatedInput("First number: ");
+	int n2 = getValidatedInput("Second number: ");
+	int n3 = getValidatedInput("Third number: ");
 
 	float a = average(n1, n2, n3);
 
